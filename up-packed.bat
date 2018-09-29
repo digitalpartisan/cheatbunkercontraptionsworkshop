c50ba9fcb7e7382cfa17142eb1623c9fe6f8fbd4
digitalpartisan <digital.partisan.86@gmail.com>
2018-09-28 18:58:12 -0700

    converted to scaffold

--- up-packed.bat
+++
@@ -1,1 +0,0 @@
-mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerContraptions - Main.ba2" "K:\FO4 Mods\CheatBunkerContraptions\Data\CheatBunkerContraptions - Main.ba2"
--- up.bat
+++
@@ -1,1 +0,0 @@
-mklink /h "K:\Steam\steamapps\common\Fallout 4\Data\CheatBunkerContraptions.esp" "K:\FO4 Mods\CheatBunkerContraptions\Data\CheatBunkerContraptions.esp"
